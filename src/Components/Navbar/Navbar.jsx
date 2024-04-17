6bbb5971002ed685b667b7fd7d1af4ddd1c31673
RakeshV0498 <rockr4674@gmail.com>
2024-04-17 20:14:58 +0530

    Updated Components for Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import logoimg from "../../assets/guvi-logo.svg";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <section className="logo-container">
+          <img src={logoimg} alt="Guvi logo" className="logo" />
+          <h1 className="title">Blog</h1>
+          <input
+            type="text"
+            className="nav-search"
+            placeholder="What do you want to read"
+          />
+        </section>
+        <button className="btn btn-signUp">Sign Up</button>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
