a5101a69743144c7eb07dea3c2e7358a3a87990f
RakeshV0498 <rockr4674@gmail.com>
2024-04-17 20:51:52 +0530

    Installed Router Package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
+  const categories = [
+    "All",
+    "Full Stack Development",
+    "Data Science",
+    "Cyber Security",
+    "Carrer",
+  ];
   return (
     <>
       <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      {categories.map((category) => (
+        <Link key={category} to={category}>
+          {category}
+        </Link>
+      ))}
     </>
   );
 }
