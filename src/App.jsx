0cb6cd55cfa96861fc0f3a927ccc546c0e000f24
RakeshV0498 <rockr4674@gmail.com>
2024-04-18 09:40:20 +0530

    Created Data for the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
 import "./App.css";
+import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const categories = [
-    "All",
-    "Full Stack Development",
-    "Data Science",
-    "Cyber Security",
-    "Carrer",
-  ];
   return (
     <>
       <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
-      {categories.map((category) => (
-        <Link key={category} to={category}>
-          {category}
-        </Link>
-      ))}
     </>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,133 @@
+const blogData = [
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2022/12/Render-array-of-objects-in-ReactJs.png",
+    description:
+      "How to Render an Array of Objects in React? [in 3 easy steps] ",
+    author: "By Tarun Singh",
+    date: "21 Mar, 2024",
+    read: "7 Min Read",
+    tag: "FSD",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2022/12/Fetch-Data-using-API-in-React.Js.png",
+    description:
+      "Use ReactJS to Fetch and Display Data from API – 5 Simple Steps",
+    author: "By Tarun Singh",
+    date: "21 Mar, 2024",
+    read: "5 Min Read",
+    tag: "FSD",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2023/06/feature-image-unique-project-ideas.jpg",
+    description: "Top 10 Unique Project Ideas for College Students ",
+    author: "By Lukesh S",
+    date: "21 Mar, 2024",
+    read: "7 Min Read",
+    tag: "FSD",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science.webp",
+    description: "Top 10 High Paying Non-Coding Jobs in Data Science in 2024",
+    author: "By Isha Sharma",
+    date: "16 Apr, 2024",
+    read: "6 Min Read",
+    tag: "DS",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2023/07/Real-World-Data-Science-Examples-2048x1072.webp",
+    description: "12 Real-World Data Science Examples: Power Of Data Science",
+    author: "By Lukesh S",
+    date: "25 Mar, 2024",
+    read: "7 Min Read",
+    tag: "DS",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2022/12/Render-array-of-objects-in-ReactJs.png",
+    description:
+      "How to Render an Array of Objects in React? [in 3 easy steps] ",
+    author: "By Tarun Singh",
+    date: "21 Mar, 2024",
+    read: "7 Min Read",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2022/06/5.-data-science-2048x1072.png",
+    description: "Can A Commerce Student Do Data Science?",
+    author: "By Saakshi Priyadarshini",
+    date: "16 Apr, 2024",
+    read: "3 Min Read",
+    tag: "DS",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Non-Coding-Jobs-in-Cybersecurity.webp",
+    description: "Non-Coding Jobs in Cybersecurity: A Comprehensive Guide",
+    author: "By Jaishree Tomar",
+    date: "26 Mar, 2024",
+    read: "4 Min Read",
+    tag: "CS",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2022/12/Yellow-Blue-Illustration-Business-Blog-Banner-2048x1152.png",
+    description: "Cybersecurity Vs Ethical Hacking: Top 10 Differences",
+    author: "By Tushar Vinocha",
+    date: "16 Apr, 2024",
+    read: "5 Min Read",
+    tag: "CS",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More.webp",
+    description: "What Is Hacking? Types of Hacking & More",
+    author: "By Meghana D",
+    date: "25 Mar, 2024",
+    read: "6 Min Read",
+    tag: "CS",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2022/12/Render-array-of-objects-in-ReactJs.png",
+    description:
+      "How to Render an Array of Objects in React? [in 3 easy steps] ",
+    author: "By Tarun Singh",
+    date: "21 Mar, 2024",
+    read: "7 Min Read",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-Feature-image-Effective-Cloud-Computing-Career-Roadmap.webp",
+    description: "Effective Cloud Computing Career Roadmap 2024",
+    author: "By Lukesh S",
+    date: "25 Mar, 2024",
+    read: "10 Min Read",
+    tag: "CC",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2023/11/Top-10-Cloud-Computing-Jobs-without-Coding-1.png",
+    description:
+      "Top 10 High Paying Cloud-Computing Jobs without Coding in 2024",
+    author: "By Isha Sharma",
+    date: "22 Mar, 2024",
+    read: "6 Min Read",
+    tag: "CC",
+  },
+  {
+    image:
+      "https://www.guvi.in/blog/wp-content/uploads/2024/01/feature_image-7.webp",
+    description: "Top 10 Cloud Computing Project Ideas [2024]",
+    author: "By Isha Sharma",
+    date: "03 Apr, 2024",
+    read: "4 Min Read",
+    tag: "CC",
+  },
+];
+
+export default blogData;
