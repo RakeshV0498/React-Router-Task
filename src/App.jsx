34570d50aff675a76368414d4418fd0c7f98d7b1
RakeshV0498 <rockr4674@gmail.com>
2024-04-18 09:57:36 +0530

    created routes for the components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import "./App.css";
 import Home from "./Components/Pages/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FSDcomp from "./Components/Pages/FSD";
+import DSComp from "./Components/Pages/DS";
+import CSComp from "./Components/Pages/CS";
+import CCComp from "./Components/Pages/CC";
 
 function App() {
   return (
@@ -10,9 +14,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/FSD" element={<FSDcomp />} />
+          <Route path="/DS" element={<DSComp />} />
+          <Route path="/CS" element={<CSComp />} />
+          <Route path="/CC" element={<CCComp />} />
         </Routes>
       </BrowserRouter>
     </>
