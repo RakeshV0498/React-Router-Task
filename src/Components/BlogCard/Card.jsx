50ec7c650a7d4c475da26aa8a07f9d5f651491b8
RakeshV0498 <rockr4674@gmail.com>
2024-04-19 11:14:23 +0530

    Updated Card compnent with the logic

--- src/Components/BlogCard/Card.jsx
+++ src/Components/BlogCard/Card.jsx
@@ -1,1 +1,32 @@
 import "/Card.css";
+import PropTypes from "prop-types";
+
+function Card({ image, description, author, date, read }) {
+  return (
+    <>
+      <article className="card-container">
+        <section className="img-container">
+          <img src={image} alt={description} className="card-img" />
+        </section>
+        <section className="data-container">
+          <p className="card-desc">{description}</p>
+          <p className="card-author">{author}</p>
+          <div className="small-content">
+            <p className="card-date">{date}</p>
+            <p className="card-read">{read}</p>
+          </div>
+        </section>
+      </article>
+    </>
+  );
+}
+
+Card.propTypes = {
+  image: PropTypes.string,
+  description: PropTypes.string,
+  author: PropTypes.string,
+  date: PropTypes.string,
+  read: PropTypes.string,
+};
+
+export default Card;
