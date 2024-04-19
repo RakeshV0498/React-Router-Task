fe0d643fbec9057a3a8e7342f8f8ffadef93aaac
RakeshV0498 <rockr4674@gmail.com>
2024-04-19 12:46:10 +0530

    Updated card component for FSD

--- src/Pages/FSD.jsx
+++ src/Pages/FSD.jsx
@@ -1,7 +1,14 @@
+import blogData from "../Data";
+import Card from "../Components/BlogCard/Card";
+
 function FSDcomp() {
   return (
     <>
-      <h1>FSD</h1>
+      {blogData
+        .filter((data) => data.tag === "FSD")
+        .map((data) => (
+          <Card key={data.id} {...data} />
+        ))}
     </>
   );
 }
