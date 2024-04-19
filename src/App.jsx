3ad0f140dd3e10da8f38077b28864026626c93e7
RakeshV0498 <rockr4674@gmail.com>
2024-04-19 09:37:08 +0530

    Moved Pages folder out of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import Home from "./Components/Pages/Home";
+import Home from "./Pages/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import FSDcomp from "./Components/Pages/FSD";
-import DSComp from "./Components/Pages/DS";
-import CSComp from "./Components/Pages/CS";
-import CCComp from "./Components/Pages/CC";
+import FSDcomp from "./Pages/FSD";
+import DSComp from "./Pages/DS";
+import CSComp from "./Pages/CS";
+import CCComp from "./Pages/CC";
 
 function App() {
   return (
