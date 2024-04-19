302f9c2077ab54f8fff59d85ee9b3f4412e1065e
RakeshV0498 <rockr4674@gmail.com>
2024-04-19 09:43:27 +0530

    Implemented Nav Components

--- src/Components/InternalNav/NavInternal.jsx
+++ src/Components/InternalNav/NavInternal.jsx
@@ -1,1 +1,24 @@
 import "./NavInternal.css";
+import { Link } from "react-router-dom";
+
+const categories = [
+  "All",
+  "FullStack Development",
+  "Data Science",
+  "Cyber Security",
+  "Cloud Computing",
+];
+
+function NavInternal() {
+  return (
+    <>
+      {categories.map((category) => (
+        <Link key={category} to={"FSD"}>
+          category
+        </Link>
+      ))}
+    </>
+  );
+}
+
+export default NavInternal;
