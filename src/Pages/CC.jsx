127ffc28f7cb5b340e61324d216276906c843460
RakeshV0498 <rockr4674@gmail.com>
2024-04-19 12:49:24 +0530

    Updated card component for CC

--- src/Pages/CC.jsx
+++ src/Pages/CC.jsx
@@ -1,7 +1,14 @@
+import Card from "../Components/BlogCard/Card";
+import blogData from "../Data";
+
 function CCComp() {
   return (
     <>
-      <h1>CC</h1>
+      {blogData
+        .filter((data) => data.tag === "CC")
+        .map((data) => (
+          <Card key={data.id} {...data} />
+        ))}
     </>
   );
 }
